fa283d22ccb80563dd0c1d3a2717c36cf4b8604d
Jonathan Davis <trunten@btinternet.com>
2022-12-10 21:06:57 +0000

    upload assets

---
+++ assets/script.js
@@ -0,0 +1,38 @@
+// Define the data for the chart
+const data = {
+  labels: ["HTML", "CSS", "JavaScript", "VBA"],
+  datasets: [
+    {
+      label: "Language (%)",
+      data: [40, 20, 5, 35,0],
+      backgroundColor: [
+        "rgba(255, 99, 132, 0.2)",
+        "rgba(54, 162, 235, 0.2)",
+        "rgba(255, 206, 86, 0.2)",
+        "rgba(75, 192, 192, 0.2)",
+        "rgba(153, 102, 255, 0.2)"
+      ],
+      borderColor: [
+        "rgba(255, 99, 132, 1)",
+        "rgba(54, 162, 235, 1)",
+        "rgba(255, 206, 86, 1)",
+        "rgba(75, 192, 192, 1)",
+        "rgba(153, 102, 255, 1)"
+      ],
+      borderWidth: 1
+    }
+  ]
+};
+
+// Get the chart canvas element
+const ctx = document.getElementById("my-chart").getContext("2d");
+
+// Create the chart using Chart.js
+const myChart = new Chart(ctx, {
+  type: "bar",
+  data: data,
+  options: {
+    responsive: true,
+    maintainAspectRatio: true,
+  }
+});
